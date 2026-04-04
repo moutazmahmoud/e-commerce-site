b4050e9705c6e10c073b2a8f3074460a2ef367c8
Moutaz <72810603+moutazmahmoud@users.noreply.github.com>
2026-04-04 11:53:21 +0200

    Use next/script for external script loading
    Replaced inline script with next/script component for better loading strategy.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import { routing } from "@/i18n/routing";
 import { getMessages, setRequestLocale } from "next-intl/server";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -74,14 +75,20 @@ export default async function RootLayout({
     <html lang={locale} dir={dir}>
       <head>
         <link rel="preconnect" href="https://images.unsplash.com" />
-        <script nowprocket data-noptimize="1" data-cfasync="false" data-wpfc-render="false" seraph-accel-crit="1" data-no-defer="1">
-          (function () {
-              var script = document.createElement("script");
-              script.async = 1;
-              script.src = 'https://tpembars.com/NTE1MDcw.js?t=515070';
-              document.head.appendChild(script);
-          })();
-        </script>
+        <Script
+            id="travelpayouts-script"
+            strategy="afterInteractive" // أو lazyOnload لو مش مستعجل عليه
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function () {
+                    var script = document.createElement("script");
+                    script.async = 1;
+                    script.src = 'https://tpembars.com/NTE1MDcw.js?t=515070';
+                    document.head.appendChild(script);
+                })();
+              `,
+            }}
+          />
       </head>
       <body
         className={`${outfit.variable} ${inter.variable} font-sans antialiased bg-background text-foreground`}
